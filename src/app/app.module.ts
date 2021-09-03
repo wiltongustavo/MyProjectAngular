97c71ea38f7a19a28c225dd8664a608ed0362ad1
wiltongustavo <wilton.gustavo.eu@hotmail.com>
2021-09-03 17:30:49 -0300

    atualizacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/share/event.service';
 @NgModule({
 
 imports: [
@@ -17,6 +18,7 @@ BrowserModule
     EventThumbnailComponent,
     NavBarComponent
   ],
+  providers: [EventService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
